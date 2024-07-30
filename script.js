59495ff1df2619df0334c5002056815207a4ceeb
Nicolas Lo Vento <106879759+nicolasloventordp@users.noreply.github.com>
2024-07-30 11:10:54 -0300

    .

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
+	emailjs.init({
+		publicKey: "QmH-HASr6BtPvF05X",
+	});
 	var switchButton = document.querySelector('.switch-button');
 	var switchBtnRight = document.querySelector('.switch-button-case.right');
 	var switchBtnLeft = document.querySelector('.switch-button-case.left');
 	var activeSwitch = document.querySelector('.active');
 	var educacion = document.querySelector('.educacion');
 	var experiencia = document.querySelector('.experiencia');
+
 function switchLeft(){
 	switchBtnRight.classList.remove('active-case');
 	switchBtnLeft.classList.add('active-case');
@@ -28,4 +32,25 @@ switchBtnLeft.addEventListener('click', function(){
 switchBtnRight.addEventListener('click', function(){
 	switchRight();
 }, false);
+
+
 });
+
+function enviarEmail(event){
+	event.preventDefault();
+	const serviceID = "service_wlegu8g";
+	const templateID = "template_3ucqt7l";
+	const templateParams = {
+		from_name: document.getElementById('from_name').value,
+		email_id: document.getElementById('email_id').value,
+		message: document.getElementById('message').value
+	};
+	emailjs.send(serviceID,templateID,templateParams).then(
+		() => {
+			alert("send");
+		},
+		(err) => {
+			alert(err);
+		} 
+	)
+}
